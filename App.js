b592592248b4e6b604a6ffd71fd85a3238a5d978
victorbruce <victorbruce82@gmail.com>
2021-04-24 09:15:21 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import packageJson from './package.json';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>rnProduction App</Text>
+      <Text style={styles.versionNumber}>{packageJson.version}</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  versionNumber: {
+    fontSize: 18,
+  },
+});
