a22e8edc5de7dab4720f8f5634fedff7c5af78fd
victorbruce <victorbruce82@gmail.com>
2021-05-06 21:23:33 +0000

    feat: setup fastlane to build and distribute app to app distribution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-import packageJson from './package.json';
+import packageJson from '../package.json';
 
 const App = () => {
   return (
