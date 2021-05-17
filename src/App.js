3f9de3fc10e0cd3aa7c048b03fc505f3cd84e399
victorbruce <victorbruce82@gmail.com>
2021-05-17 09:06:36 +0000

    update: bump js, android and ios version numbers

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import packageJson from '../package.json';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>rnProduction App</Text>
+      <Text style={styles.title}>rnProduction App 🔥</Text>
       <Text style={styles.versionNumber}>{packageJson.version}</Text>
     </View>
   );
